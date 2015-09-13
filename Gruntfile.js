97e8d9daa6bb87b9ee8293ca2d3bab6bd4b9a69a
paras <rockypro92@gmail.com>
2015-09-13 19:26:33 +0530

    Reorganising static assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,25 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-  sass: {                              // Task 
-    dist: {                            // Target 
-      options: {                       // Target options 
-        style: 'expanded'
-      },
-      files: {                         // Dictionary of files 
-        'styles.css': 'styles.scss'       // 'destination': 'source' 
-      }
-    }
-  },
-  watch: {
-    css: {
-      files: ['*.scss'],
-      tasks: ['sass']
-    }
-  },
-  'http-server': {
-    'dev': {
-      root: './',
-      port: 8282, 
-      host: "0.0.0.0", 
-      showDir : true,
-      autoIndex: true, 
-            // server default file extension 
-            ext: "html",
-            // run in parallel with other tasks 
-            runInBackground: false,
-            // specify a logger function. By default the requests are 
-            // sent to stdout. 
-            logFn: function(req, res, error) { },
-            /// Use 'https: true' for default module SSL configuration 
-            /// (default state is disabled) 
+    sass: {                              // Task 
+      dist: {                            // Target 
+        options: {                       // Target options 
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files 
+          'assets/stylesheets/common.css': 'assets/stylesheets/src/common.scss',       // 'destination': 'source' 
+          'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss'           // 'destination': 'source' 
         }
+      }
+    },
+    watch: {
+      css: {
+        files: ['assets/stylesheets/src/*.scss'],
+        tasks: ['sass']
+      }
     }  
   });
 
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-http-server');
-grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass', 'watch']);
 };
